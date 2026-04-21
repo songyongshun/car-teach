952350de16bb03aaff49c90622b3d79744c4b098
Yongshun Song <ys.song320@gmail.com>
2026-04-21 18:17:49 +0800

    专业内容添加

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Docs;
+
+namespace MySite
+{
+  public class Program
+  {
+    public static async Task<int> Main(string[] args) =>
+      await Bootstrapper
+        .Factory
+        .CreateDocs(args)
+	      .AddSetting(Keys.Host, "ciit-linux.netlify.app")
+        .RunAsync();
+  }
+}
